a65df36ca7a38808c916302b2ddd80b2a18a87a8
Sumitprajapatintv <sumit963605@gmail.com>
2024-04-02 23:01:09 +0530

    Insertion Sort

--- src/SearchingAndSorting/BubbleSort.java
+++ src/SearchingAndSorting/BubbleSort.java
@@ -6,9 +6,33 @@ import java.util.Arrays;
 public class BubbleSort {
     public static void main(String[] args) {
         int [] arr={9,8,7,6,5,4,3,2,1};
-        Bubble(arr);
+        Insertion(arr);
         System.out.println(Arrays.toString(arr));
 
+    }
+
+    static void Insertion(int[] arr)
+    {
+        for(int i=0;i<arr.length-1;i++)
+        {
+            for(int j=i+1;j>0;j--)
+            {
+                if(arr[j]<arr[j-1])
+                {
+                    swap(arr,j,j-1);
+                }
+                else {
+                    break;
+                }
+            }
+        }
+    }
+    static void swap(int[] arr,int first,int second)
+    {
+        int temp;
+        temp=arr[first];
+        arr[first]=arr[second];
+        arr[second]=temp;
     }
     static void Bubble(int [] arr)
     {
