fef7cf88bb82af3a623b0a8dc398db68922e0858
Sumitprajapatintv <sumit963605@gmail.com>
2024-05-14 23:26:17 +0530

    Complete the Lucky Number in MAtrix

--- src/Arrays/LuckeyNumberInMatrix.java
+++ src/Arrays/LuckeyNumberInMatrix.java
@@ -6,8 +6,8 @@ import java.util.List;
 
 public class LuckeyNumberInMatrix {
     public static void main(String[] args) {
-        int[][] arr={{3, 7, 8},{9,11,13},{15,16,17}};
-        System.out.println((luckyNumbers(arr)));
+        int[][] arr={{7,8},{1,2}};
+        System.out.println(luckyNumbers(arr));
     }
     static public List<Integer> luckyNumbers (int[][] matrix) {
         List<Integer> arr2=new ArrayList<>();
@@ -15,32 +15,32 @@ public class LuckeyNumberInMatrix {
         int colLength=matrix[0].length;
         int[] row=new int[rowLength];
         int[] col=new int[colLength];
-        for(int i=0;i<matrix.length;i++)
-        {
-            int min=Integer.MAX_VALUE;
-            int max=Integer.MIN_VALUE;
-            for (int j=0;j<matrix[0].length;j++)
-            {
-                if(matrix[i][j]<min)
-                {
-                    min=matrix[i][j];
+        int p=0,q=0;
+        for (int i = 0; i < rowLength; i++) {
+            int min = Integer.MAX_VALUE;
+            for (int j = 0; j < colLength; j++) {
+                if (matrix[i][j] < min) {
+                    min = matrix[i][j];
                 }
-
-                if(matrix[j][i]>max)
-                {
-                    max=matrix[j][i];
-                }
-                col[j]=max;
             }
-            row[i]=min;
-
-
+            row[p++] = min;
         }
-        for(int i=0;i<rowLength;i++)
+
+        // Find the maximum element in each column
+        for (int j = 0; j < colLength; j++) {
+            int max = Integer.MIN_VALUE;
+            for (int i = 0; i < rowLength; i++) {
+                if (matrix[i][j] > max) {
+                    max = matrix[i][j];
+                }
+            }
+            col[q++] = max;
+        }
+        for(int i=0;i<row.length;i++)
         {
-            for(int j=0;j<colLength;j++)
+            for(int j=0;j<col.length;j++)
             {
-                if(row[i]==col[j]);
+                if(row[i]==col[j])
                 {
                     arr2.add(row[i]);
                 }
